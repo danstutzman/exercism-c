9ecd710f43e54158f3f65db074cb1362960f8071
Dan Stutzman <dan@danstutzman.com>
2021-03-27 22:00:13 -0600

    Pass test_word_count_multiple_occurrences...

--- word-count/src/word_count.c
+++ word-count/src/word_count.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "word_count.h"
 
-#define DEBUG 1
+#define DEBUG 0
 
 int word_count(const char *input_text, word_count_word_t * words) {
   if (DEBUG) {
@@ -12,38 +12,47 @@ int word_count(const char *input_text, word_count_word_t * words) {
 
   const char* start_of_word = input_text;
   int current_word_len = 0;
-  int num_word = 0;
+  int num_words_so_far = 0;
   while (1) {
     char c = start_of_word[current_word_len];
-    //printf("num_word=%d current_word_len=%d c=%c\n",
-    //    num_word, current_word_len, c);
     if (c == ' ' || c == '\0') {
-      strncpy(words[num_word].text, start_of_word, current_word_len);
-      words[num_word].text[current_word_len] = '\0';
-      words[num_word].count = 1;
+      int found_word = 0;
+      for (int i = 0; i < num_words_so_far; i++) {
+        if (strncmp(words[i].text, start_of_word, current_word_len) == 0) {
+          strncpy(words[i].text, start_of_word, current_word_len);
+          words[i].text[current_word_len] = '\0';
+          words[i].count += 1;
+          found_word = 1;
+          break;
+        }
+      }
+
+      if (!found_word) {
+        strncpy(words[num_words_so_far].text, start_of_word, current_word_len);
+        words[num_words_so_far].text[current_word_len] = '\0';
+        words[num_words_so_far].count = 1;
+        num_words_so_far += 1;
+      }
+
+      start_of_word += current_word_len + 1;
+      current_word_len = 0;
 
       if (c == '\0') {
         break;
-      } else {
-        num_word += 1;
-        start_of_word += current_word_len + 1;
-        current_word_len = 0;
       }
     } else {
-      //words[num_word].text[offset_into_word] = input_text[offset];
       current_word_len += 1;
     }
   }
-  int num_words = num_word + 1;
 
   if (DEBUG) {
-    for (int i = 0; i < num_words; i++) {
+    for (int i = 0; i < num_words_so_far; i++) {
       printf("words[%d].count = %d\n", i, words[i].count);
       printf("words[%d].text = %s\n", i, words[i].text);
     }
   }
 
-  return num_words;
+  return num_words_so_far;
 }
 
 //int main() { printf("test\n"); }
--- word-count/test/test_word_count.c
+++ word-count/test/test_word_count.c
@@ -82,7 +82,6 @@ void test_word_count_one_of_each_word(void)
 
 void test_word_count_multiple_occurrences_of_a_word(void)
 {
-   TEST_IGNORE();
    int index = 0;
    int actual_word_count;
    char *input_text = "one fish two fish red fish blue fish";
