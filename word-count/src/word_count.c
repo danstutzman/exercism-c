3c5544a7f4e8ae4e9a2deb98ce8f44c7999ac672
Dan Stutzman <dan@danstutzman.com>
2021-03-27 20:31:20 -0600

    Added main function

---
+++ word-count/src/word_count.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "word_count.h"
+
+int word_count(const char *input_text, word_count_word_t * words) {
+  (void)input_text; // unused
+  (void)words; // unused
+  return 0;
+}
+
+int main() {
+  printf("test\n");
+}
